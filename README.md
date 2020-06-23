720b858ab402f836635282c7af44f2f99c6eeb6a
Mx-Lily <44614832+Mx-Plume@users.noreply.github.com>
2020-06-23 20:58:44 +0200

    Add Script for Run a Discord Bot

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MrClean.sh
 - Supprime les fichiers temporaires
+
+# RunBotDiscord
+- Permet au bot discord de fonctionner h24 sur un vps
---
+++ RunBotDiscord.sh
@@ -0,0 +1,4 @@
+sudo systemctl start bot.service
+sudo systemctl stop bot.service
+sudo systemctl status bot.service
+sudo systemctl reload bot.service
